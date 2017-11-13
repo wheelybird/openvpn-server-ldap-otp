6d03f445ac6684ac6fd356ef92ce5ec18530519f
Brian Lycett <brian@wheelybird.co.uk>
2017-11-13 13:24:45 +0000

    Fix NAT and routing issues

--- files/configuration/setup_networking.sh
+++ files/configuration/setup_networking.sh
@@ -2,20 +2,16 @@
 
 mkdir -p /dev/net
 if [ ! -c /dev/net/tun ]; then
-    mknod /dev/net/tun c 10 200
+ mknod /dev/net/tun c 10 200
 fi
 
-#Set up routes to push to the client and create NAT rules for those
-#routes if NAT is required.
+ovpn_net_net=`whatmask ${OVPN_NETWORK} | grep 'Network Address' | awk '{ print $5 }'`
+ovpn_net_cidr=`whatmask ${OVPN_NETWORK} | grep 'CIDR' | awk '{ print $4 }'`
+ovpn_net="${ovpn_net_net}${ovpn_net_cidr}"
 
-network_addr=`whatmask ${OVPN_NETWORK} | grep 'IP Entered' | awk '{ print $5 }'`
-cidr=`whatmask ${OVPN_NETWORK} | grep 'CIDR' | awk '{ print $4 }'`
-
-#first_ip=`whatmask ${OVPN_NETWORK} | grep 'First Usable IP Address' | awk '{ print $7 }'`
-#netmask=`whatmask ${OVPN_NETWORK} | grep 'Netmask =' | awk '{ print $4 }'`
-
-export this_ovpn_network="$network_addr$cidr"
 export this_natdevice=`route | grep '^default' | grep -o '[^ ]*$'`
+
+#Set up routes to push to the client.
 
 if [ "${OVPN_ROUTES}x" != "x" ] ; then
 
@@ -27,16 +23,17 @@ if [ "${OVPN_ROUTES}x" != "x" ] ; then
   for this_route in ${route_list[@]} ; do
 
    echo "routes: adding route $this_route to server config"
-   echo "push route \"$this_route\"" >> /tmp/ovpn_routes.txt
+   echo "push \"route $this_route\"" >> /tmp/routes_config.txt
 
    if [ "$OVPN_NAT" == "true" ]; then
     IFS=" "
-    this_ip=`whatmask $this_route | grep 'IP Entered' | awk '{ print $5 }'`
+    this_net=`whatmask $this_route | grep 'Network Address' | awk '{ print $5 }'`
     this_cidr=`whatmask $this_route | grep 'CIDR' | awk '{ print $4 }'`
     IFS=","
-    to_masquerade="${this_ip}${this_cidr}"
-    echo "iptables: setting masquerade rule for route $to_masquerade via $this_natdevice"
-    iptables -t nat -C POSTROUTING -s "$to_masquerade" -o $this_natdevice -j MASQUERADE || iptables -t nat -A POSTROUTING -s "$to_masquerade" -o $this_natdevice -j MASQUERADE
+    to_masquerade="${this_net}${this_cidr}"
+    echo "iptables: masquerade from $ovpn_net to $to_masquerade via $this_natdevice"
+    iptables -t nat -C POSTROUTING -s "$ovpn_net" -d "$to_masquerade" -o $this_natdevice -j MASQUERADE || \
+    iptables -t nat -A POSTROUTING -s "$ovpn_net" -d "$to_masquerade" -o $this_natdevice -j MASQUERADE
    fi
 
   done
@@ -46,11 +43,13 @@ if [ "${OVPN_ROUTES}x" != "x" ] ; then
 else
 
  #If no routes are set then we'll redirect all traffic from the client over the tunnel.
- #This means that we'll set up NAT regardless of whether OVPN_NAT is set to false.
 
  echo "push \"redirect-gateway def1\"" >> /tmp/routes_config.txt
+ echo "iptables: masquerade from $ovpn_net to everywhere via $this_natdevice"
 
- echo "iptables: setting masquerade rule for all VPN clients ($this_ovpn_network) via $this_natdevice"
- iptables -t nat -C POSTROUTING -s $this_ovpn_network -o $this_natdevice -j MASQUERADE || iptables -t nat -A POSTROUTING -s $this_ovpn_network -o $this_natdevice -j MASQUERADE
+ if [ "$OVPN_NAT" == "true" ]; then
+  iptables -t nat -C POSTROUTING -s "$ovpn_net" -o $this_natdevice -j MASQUERADE || \
+  iptables -t nat -A POSTROUTING -s "$ovpn_net" -o $this_natdevice -j MASQUERADE
+ fi
 
 fi
