5cda873519124c8286ed401e5e85e14a4e488689
Christoph Honal <christoph.honal@web.de>
2020-05-28 12:02:48 +0200

    Allow manual configuration (#35)
    * Add env variable to append extra options to server config
    
    * Add option to configure server by hand

--- files/configuration/set_defaults.sh
+++ files/configuration/set_defaults.sh
@@ -43,6 +43,7 @@ if [ "${OVPN_ENABLE_COMPRESSION}x" == "x" ];      then export OVPN_ENABLE_COMPRE
 if [ "${REGENERATE_CERTS}x" == "x" ];             then export REGENERATE_CERTS="false";                   fi
 if [ "${OVPN_MANAGEMENT_ENABLE}x" == "x" ];       then export OVPN_MANAGEMENT_ENABLE="false";             fi
 if [ "${OVPN_MANAGEMENT_NOAUTH}x" == "x" ];       then export OVPN_MANAGEMENT_NOAUTH="false";             fi
+if [ "${OVPN_DEFAULT_SERVER}x" == "x" ];          then export OVPN_DEFAULT_SERVER="true";                 fi
 if [ "${DEBUG}x" == "x" ];                        then export DEBUG="false";                              fi
 if [ "${LOG_TO_STDOUT}x" == "x" ];                then export LOG_TO_STDOUT="true";                       fi
 if [ "${ENABLE_OTP}x" == "x" ];                   then export ENABLE_OTP="false";                         fi
